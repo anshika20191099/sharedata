f4997138061b9af1e720cc403c0742072aba98bb
anshika20191099 <anshikamishrakanpur123456@gmail.com>
2020-05-17 12:57:54 +0530

    change1

--- app/src/main/java/com/example/myprog/MainActivity.java
+++ app/src/main/java/com/example/myprog/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.myprog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText ed1,ed2,ed3;
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=findViewById(R.id.empsav);
+        ed2=findViewById(R.id.invest);
+        ed3=findViewById(R.id.amount);
+
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        }
+        public void empsav(double d) {
+            double savings = Double.parseDouble(ed1.getText().toString());
+            double amt;
+            double m=invest();
+            amt = ((savings) * m) / 100;
+            ed3.setText(String.valueOf(amt));
+        }
+    public double invest() {
+        double perinvest = Double.parseDouble(ed1.getText().toString());
+        return (perinvest);
     }
 }
